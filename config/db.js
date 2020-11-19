1f6d079f18ca49c52d8706de16608051df1f0968
Pratyay Rudra <rudrapratyay1999@gmail.com>
2020-11-19 12:01:53 +0530

    mysql keepalive

--- config/db.js
+++ config/db.js
@@ -2,20 +2,17 @@ const mysql = require('mysql');
 
 var mysqlConnection = mysql.createConnection(process.env.DATABASE_URL);
 
-handleConnection = () => {
-    mysqlConnection.connect((err) => {
-        if (!err) {
-            console.log("Connected to database");
-        } else {
-            console.log('Connection failed');
-        }
-    })
-}
-
-handleConnection();
-
-mysqlConnection.on('error', () => {
-    handleConnection();
+mysqlConnection.connect((err) => {
+    if (!err) {
+        console.log("Connected to database");
+    } else {
+        console.log('Connection failed');
+    }
 })
+
+//To keep the mysql connection alive
+setInterval(function () {
+    mysqlConnection.query('SELECT 1');
+}, 5000);
 
 module.exports = mysqlConnection;
