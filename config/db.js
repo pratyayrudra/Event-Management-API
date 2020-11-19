b9d95b6aaa6032d601e0c4ae92a74787cf96f6d6
Pratyay Rudra <rudrapratyay1999@gmail.com>
2020-11-19 11:55:00 +0530

    nodemon added

--- config/db.js
+++ config/db.js
@@ -2,12 +2,20 @@ const mysql = require('mysql');
 
 var mysqlConnection = mysql.createConnection(process.env.DATABASE_URL);
 
-mysqlConnection.connect((err) => {
-    if (!err) {
-        console.log("Connected to database");
-    } else {
-        console.log('Connection failed');
-    }
+handleConnection = () => {
+    mysqlConnection.connect((err) => {
+        if (!err) {
+            console.log("Connected to database");
+        } else {
+            console.log('Connection failed');
+        }
+    })
+}
+
+handleConnection();
+
+mysqlConnection.on('error', () => {
+    handleConnection();
 })
 
 module.exports = mysqlConnection;
